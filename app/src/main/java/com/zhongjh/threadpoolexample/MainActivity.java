3a9da913015d10ecad4a2583521f79c05bb42af6
zhongjh <254191389@qq.com>
2021-05-17 18:19:04 +0800

    添加演示代码

--- app/src/main/java/com/zhongjh/threadpoolexample/MainActivity.java
+++ app/src/main/java/com/zhongjh/threadpoolexample/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
@@ -65,7 +66,11 @@ public class MainActivity extends AppCompatActivity {
                 testWaitNotify();
                 break;
             case R.id.btnNewThreadPool:
-                newThreadPool();
+                newThreadPool(1, 1,
+                        0L, TimeUnit.MILLISECONDS,
+                        null,
+                        null,
+                        null);
                 break;
             default:
                 break;
@@ -294,19 +299,19 @@ public class MainActivity extends AppCompatActivity {
             if (!beShouldSub) {
                 // 等待
                 try {
-                    Log.d("testWaitNotify","子线程等待lock");
+                    Log.d("testWaitNotify", "子线程等待lock");
                     lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             for (int j = 0; j < 2; j++) {
-                Log.d("testWaitNotify","子循环第" + (j + 1) + "次");
+                Log.d("testWaitNotify", "子循环第" + (j + 1) + "次");
             }
             // 子线程执行完毕，子线程标志位设为false
             beShouldSub = false;
             // 唤醒
-            Log.d("testWaitNotify","子线程唤醒lock");
+            Log.d("testWaitNotify", "子线程唤醒lock");
             lock.notify();
         }
     }
@@ -319,19 +324,19 @@ public class MainActivity extends AppCompatActivity {
             if (beShouldSub) {
                 // 等待
                 try {
-                    Log.d("testWaitNotify","主线程等待lock");
+                    Log.d("testWaitNotify", "主线程等待lock");
                     lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             for (int j = 0; j < 3; j++) {
-                Log.d("testWaitNotify","主循环第" + (j + 1) + "次");
+                Log.d("testWaitNotify", "主循环第" + (j + 1) + "次");
             }
             // 主线程执行完毕，子线程标志位设为true
             beShouldSub = true;
             // 唤醒
-            Log.d("testWaitNotify","主线程唤醒lock");
+            Log.d("testWaitNotify", "主线程唤醒lock");
             lock.notify();
         }
     }
@@ -339,12 +344,34 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 创建线程池
      *
-     *
+     * @param corePoolSize    核心线程数：
+     *                        线程池新建线程的时候，如果当前线程总数小于corePoolSize，则新建的是核心线程，如果超过corePoolSize，则新建的是非核心线程；
+     *                        核心线程默认情况下会一直存活在线程池中，即使这个核心线程啥也不干(闲置状态)；
+     *                        如果设置了 allowCoreThreadTimeOut 为 true，那么核心线程如果不干活(闲置状态)的话，超过一定时间(时长下面参数决定)，就会被销毁掉。
+     * @param maximumPoolSize 线程池能容纳的最大线程数量：
+     *                        线程总数 = 核心线程数 + 非核心线程数。
+     * @param keepAliveTime   非核心线程空闲存活时长：
+     *                        非核心线程空闲时长超过该时长将会被回收，主要应用在缓存线程池中。
+     *                        当设置了 allowCoreThreadTimeOut 为 true 时，对核心线程同样起作用。
+     * @param unit            keepAliveTime 的单位：
+     *                        它是一个枚举类型，常用的如：TimeUnit.SECONDS（秒）、TimeUnit.MILLISECONDS（毫秒）。
+     * @param workQueue       任务队列：
+     *                        当所有的核心线程都在干活时，新添加的任务会被添加到这个队列中等待处理，如果队列满了，则新建非核心线程执行任务，常用的workQueue 类型：
+     * @param threadFactory   线程工厂：
+     *                        用来创建线程池中的线程，通常用默认的即可。
+     * @param handler         拒绝策略：
+     *                        在线程池已经关闭的情况下和任务太多导致最大线程数和任务队列已经饱和，无法再接收新的任务，
+     *                        在上面两种情况下，只要满足其中一种时，在使用 execute() 来提交新的任务时将会拒绝
+     * @return ThreadPoolExecutor 线程池
      */
-    private void newThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
-                               BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
-//        ThreadPoolExecutor executorService = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60, TimeUnit.SECONDS,
-//                new SynchronousQueue<Runnable>(), Util.threadFactory("OkHttp Dispatcher", false));
+    private ThreadPoolExecutor newThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
+                                             BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        try {
+            return new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit,
+                    workQueue, threadFactory, handler);
+        } catch (Throwable ex) {
+            // 因为demo传null，所以会报错，只是讲解参数意义
+            return null;
+        }
     }
-
 }
